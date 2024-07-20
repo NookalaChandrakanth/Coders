6f0853ddfeb65dc081ac9ce76496aeb6f2c70aa4
NookalaChandrakanth <nookalachandrakanth@gmail.com>
2024-07-20 18:30:54 +0530

    Add files via upload

---
+++ StudentAge.java
@@ -0,0 +1,25 @@
+package first_assignment;
+
+
+public class StudentAge
+
+
+{
+static void add()
+{ 
+	int a = 30;
+	int b = 40;
+	int multi = a*b;
+	System.out.println(multi);
+}
+
+public static void main(String[] args) 
+{
+	String location = "kurnool";
+
+
+System.out.println(location );
+
+add();
+}
+}
